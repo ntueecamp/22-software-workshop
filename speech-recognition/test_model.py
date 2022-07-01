523a774cfdb4d306aad619fbf0c76dc848ef5787
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-07-01 11:20:00 +0800

    hot fix: add choose device dialog to testing script, overlap samples to increase accuracy

--- speech-recognition/test_model.py
+++ speech-recognition/test_model.py
@@ -6,16 +6,38 @@ from tensorflow import keras
 import sounddevice as sd
 import samplerate
 import time
+import os
 
-input_device = 2
+# clear terminal
+if os.name == "posix":
+    os.system("clear")
+else:
+    os.system("cls")
+
+device_list = sd.query_devices().__repr__()
+print("\n" + device_list + "\n")
+num_devices = len(device_list.split("\n"))
+while True:
+    input_device = input(
+        f"select input device (0-{num_devices}): [{sd.default.device[0]}] "
+    )
+    try:
+        input_device = int(input_device)
+        if input_device < num_devices and input_device >= 0:
+            break
+    except:
+        pass
+    print("invalid input, please try again")
+
 device_info = sd.query_devices(input_device)
+print(device_info)
 config = {
     "device": input_device,
     "channels": device_info["max_input_channels"],
     "samplerate": int(device_info["default_samplerate"]),
-    "blocksize": 2048,
+    "blocksize": 4096,
 }
-MODEL_fs = 16000
+MODEL_FS = 16000
 
 
 def get_spectrogram(audio):
@@ -61,35 +83,46 @@ def predict(spec):
     answers = ["forward", "backward", "left", "right", "invalid"]
     results = model.predict_on_batch(spec)
     answer_ind = int(results.argmax(axis=1))
-    if results[0, answer_ind] > 0.7:
-        print(answers[answer_ind])
-    return results
+    # only return the answer if it is above a certain threshold and is not invalid, otherwise return None
+    THRESHOLD = 0.7
+    if results[0, answer_ind] > THRESHOLD and answer_ind != 4:
+        return answers[answer_ind]
+    return None
 
 
 audio = np.array([])
+last_result = None
 
 
 def audio_callback(indata, frames, time, status):
-    global audio
+    global audio, last_result
 
     if status:
         print(status)
 
     audio = np.append(audio, indata)
-    if audio.shape[0] > config["samplerate"]:
-        data = audio[: config["samplerate"]]
-        audio = audio[config["samplerate"] :]
+    if audio.shape[0] > config["samplerate"] * 1:
+        data = audio[: config["samplerate"] * 1]
+        # make the data we use for prediction to overlap with each other
+        audio = audio[config["samplerate"] // 2 :]
 
-        data = samplerate.resample(data, MODEL_fs / config["samplerate"], "sinc_best")
+        data = samplerate.resample(data, MODEL_FS / config["samplerate"], "sinc_best")
+
         data = data - np.mean(data)
         data = data / np.max(np.abs(data))
+        
         data = np.tile(data.reshape(-1, 1), (1, 2))
         spec = get_spectrogram(data)
-        predict(spec)
+        
+        current_result = predict(spec)
+        if last_result != current_result:
+            if current_result is not None:
+                print(current_result)
+            last_result = current_result
 
 
 def main():
-    duration = 10
+    duration = 30
 
     stream = sd.InputStream(
         device=config["device"],
